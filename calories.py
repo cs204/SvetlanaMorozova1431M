f5ab224c22c8852a8a38e026301a3025241a11f2
test <test>
2023-12-24 00:47:59 +0700

    test

---
+++ calories.py
@@ -0,0 +1,28 @@
+food_values = {
+    'Apple': 130,
+    'Avocado': 50,
+    'Banana': 110,
+    'Cantaloupe': 50,
+    'Grapefruit': 60,
+    'Grapes': 90,
+    'Honeydew Melon': 50,
+    'Kiwifruit': 90,
+    'Lemon': 15,
+    'Lime': 20,
+    'Nectarine': 60,
+    'Orange': 80,
+    'Peach': 60,
+    'Pear': 10,
+    'Pineapple': 50,
+    'Plums': 70,
+    'Strawberries': 50,
+    'Sweet Cherries': 100,
+    'Tangerine': 50,
+    'Watermelon': 80
+}
+fruit = input("Фрукт: ")
+if fruit in food_values:
+    calories = food_values[fruit]
+    print(f"Калории: {calories}")
+else:
+    print("Фрукт не найден")
